670bdcc2bb2e603d52a9c40803701eefbf736207
hoshikwyw <khaingwutyiwin1712@gmail.com>
2024-11-08 15:56:56 +0630

    Setup Tunnel Scrolling

---
+++ assets/js/data.js
@@ -0,0 +1,44 @@
+const slidesData = [
+    {
+        title: "Neon",
+        id: "d1",
+    },
+    {
+        title: "Volt",
+        id: "d2",
+    },
+    {
+        title: "Amber",
+        id: "d3",
+    },
+    {
+        title: "Crimson",
+        id: "d4",
+    },
+    {
+        title: "Emerald",
+        id: "d5",
+    },
+    {
+        title: "Indigo",
+        id: "d6",
+    },
+    {
+        title: "Cyan",
+        id: "d7",
+    },
+    {
+        title: "Cyan",
+        id: "d8",
+    },
+    {
+        title: "Cyan",
+        id: "d9",
+    },
+    {
+        title: "Cyan",
+        id: "d10",
+    }
+]
+
+export default slidesData
